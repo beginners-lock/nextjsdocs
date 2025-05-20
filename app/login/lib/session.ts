4d7edc175758fb3df475f06bf806e9a0c0b85f84
Chukwu Rophi <chukwurophi@gmail.com>
2025-05-20 06:14:39 +0100

    feat: setup the session functions

---
+++ app/login/lib/session.ts
@@ -0,0 +1,42 @@
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const secretkey = new TextEncoder().encode(SECRET_KEY);
+
+type PayLoad = {
+    userId: string
+    expiresAt: Date
+}
+
+export const createSession = async (userId: string) => {
+    const expiresAt = new Date(Date.now() + 7*24*60*60*1000);
+    const payload = { userId, expiresAt };
+
+    const session = await encrypt(payload);
+
+    const cook = await cookies()
+    cook.set("session", session, {
+        httpOnly: true,
+        expires: expiresAt,
+        secure: true
+    })
+}
+
+const encrypt = async (payload: PayLoad) => {
+    return new SignJWT(payload).setProtectedHeader({
+        alg: "HS256"
+    }).setIssuedAt().setExpirationTime("7d").sign(secretkey)
+}
+
+export const decrypt = async (session: string | undefined = "") => {
+    try{
+        const { payload } = await jwtVerify(session, secretkey, {
+            algorithms: ["HS256"]
+        })
+
+        return { payload }
+    }catch(e){
+        console.log('Decryption error: '+e);
+    }
+}
